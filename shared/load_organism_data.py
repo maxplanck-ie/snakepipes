39a1f4084cd874a5b338d5c2a2e70f0bac37c975
asrichter <asrichter@users.noreply.github.com>
2016-06-23 19:34:36 +0200

    moved output to main Snakefile

--- shared/load_organism_data.py
+++ shared/load_organism_data.py
@@ -1,13 +1,7 @@
+### Read organism configuration file and set organism-specific variables #######
+################################################################################
 try:
     include: os.path.join(maindir, "shared", "organisms", genome+".py")
 except:
-    print("Error! Organism-specific configuration file NOT found for:", genome, "\n")
+    print("ERROR: Organism-specific configuration file NOT found for:", genome, "\n")
     exit(1)
-
-if "verbose" in config and config["verbose"]==True:
-    print("--- Genome ---------------------------------------------------------------------")
-    print("Genome:", genome)
-    print("Effective genome size:", genome_size)
-    print("Bowtie2 index:", bowtie2_index)
-    print("Gene annotation GTF:", genes_gtf)
-    print("-" * 80)
