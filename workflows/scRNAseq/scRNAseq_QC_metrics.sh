0d03e33cf15d1778870894b7c4ffbfdd39208665
Steffen Heyne <heyne@ie-freiburg.mpg.de>
2016-12-18 00:05:05 +0100

    up

--- workflows/scRNAseq/scRNAseq_QC_metrics.sh
+++ workflows/scRNAseq/scRNAseq_QC_metrics.sh
@@ -8,7 +8,7 @@ mkdir -p $dir/Results/QC
 
 for i in  $dir/Counts/*cout{b,c}.csv; do 
  out=$(echo $i | sed 's/.*\///'); 
-	echo $out;
+	echo $out 1>&2;
  cat $i | awk -v file=$out '{if (NR==1) {next;}; for (i=2;i<=NF;i++) COUNTS[i-1]+=$i;} \
   END{for (i=1;i<=192;i++){if (i<=96) sum1+=COUNTS[i];else sum2+=COUNTS[i];} if (sum1>sum2) offset=1; else offset=97; \
       for (i=offset;i<offset+96;i++) {OFS="\t";print file,i,COUNTS[i];sum+=COUNTS[i];}}' >$dir/Results/QC/$out.cellsum;
@@ -18,7 +18,7 @@ paste <(cat $dir/Results/QC/*coutc*.cellsum) <(cat $dir/Results/QC/*coutb*.cells
 
 cat  $dir/Results/all_samples.cellsum_coutc_countb.tsv | awk '{if (NR==1) last=$1; \
    if ($1==last) {sum1+=$3; sum2+=$6;} else {print last,sum1,sum2,sum2/sum1,sum1/96,sum2/96; sum1=0; sum2=0}; last=$1} \
-   END{print last,sum1,sum2,sum2/sum1,sum1/96,sum2/96;}' 
+   END{print last,sum1,sum2,sum2/sum1,sum1/96,sum2/96;}'
 
 #> $dir/Results/QC_report.all_samples.tsv
 
