6b08ae85bf717798222be756c2fc7cc96bf939eb
Steffen Heyne <heyne@ie-freiburg.mpg.de>
2016-12-20 17:26:34 +0100

    up

--- workflows/scRNAseq/scRNAseq_QC_metrics.sh
+++ workflows/scRNAseq/scRNAseq_QC_metrics.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-dir=$1
+dir_in=$1
+dir_out=$2
 
 dir=$(realpath $dir)
 
-mkdir -p $dir/Results/QC
+mkdir -p $dir_out/data
 
-for i in  $dir/Counts/*11wks_1*cout{b,c}.csv; do 
+for i in  $dir_in/*11wks_1*cout{b,c}.csv; do 
  out=$(echo $i | sed 's/.*\///'); 
 	echo $out 1>&2;
  cat $i | awk -v file=$out '{if (NR==1) {next;}; for (i=2;i<=NF;i++) COUNTS[i-1]+=$i;} \
   END{for (i=1;i<=192;i++){if (i<=96) sum1+=COUNTS[i];else sum2+=COUNTS[i];} if (sum1>sum2) offset=1; else offset=97; \
-      for (i=offset;i<offset+96;i++) {OFS="\t";print file,i,COUNTS[i];sum+=COUNTS[i];}}' >$dir/Results/QC/$out.cellsum;
+      for (i=offset;i<offset+96;i++) {OFS="\t";print file,i,COUNTS[i];sum+=COUNTS[i];}}' >$dir_out/data/$out.cellsum;
 done
 
 ## with header!
-paste <(echo -e "sample\tcell\tcell_reads"; cat $dir/Results/QC/*coutc*.cellsum) <(echo -e "sample2\tcell2\tcell_transcripts"; cat $dir/Results/QC/*coutb*.cellsum) > $dir/Results/all_samples.cellsum_coutc_countb.tsv
+paste <(echo -e "sample\tcell\tcell_reads"; cat $dir_out/data/*coutc*.cellsum) <(echo -e "sample2\tcell2\tcell_transcripts"; cat $dir_out/data/*coutb*.cellsum) > $dir_out/all_samples.cellsum_coutc_countb.tsv
 
 ## summary per sample/lib
-cat  $dir/Results/all_samples.cellsum_coutc_countb.tsv | awk '{if (NR<=2) last=$1; \
+cat  $dir_out/all_samples.cellsum_coutc_countb.tsv | awk '{if (NR<=2) last=$1; \
    if ($1==last) {sum1+=$3; sum2+=$6;} else {print last,sum1,sum2,sum2/sum1,sum1/96,sum2/96; sum1=0; sum2=0}; last=$1} \
    END{print last,sum1,sum2,sum2/sum1,sum1/96,sum2/96;}'
 
