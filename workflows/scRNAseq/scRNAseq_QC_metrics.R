9cd65ee25fece0be8b27064ff20a3730b8e54f5d
Steffen Heyne <heyne@ie-freiburg.mpg.de>
2016-12-20 17:07:56 +0100

    addd more QC plots

--- workflows/scRNAseq/scRNAseq_QC_metrics.R
+++ workflows/scRNAseq/scRNAseq_QC_metrics.R
@@ -1,5 +1,105 @@
+##########################################################################
+## script for merging several coutt files with single cell data 
+## and output the combined data to file
+##
+## Author: Steffen Heyne MPI-IE Freiburg
+
+## call example: /package/R-3.3.1/bin/Rscript my_scRNA_path/ beta_cells_28wks.merged.coutt.tsv 1
+
+require(ggplot2)
+require(dplyr)
+require(scales)
+
+args <- commandArgs(trailingOnly=TRUE)
 
 
-count_comp_11wks_genomic=read.csv("/data/pospisilik/group/heyne/laura/Project_A564+A583/B6_11wks_all_genomic.coutc_countb.tsv",header = F,sep = "\t",stringsAsFactors = F)
 
-ggplot(dat=count_comp_11wks_genomic,aes(x=(V3),y=(V6),color=V1))+geom_point()+facet_wrap(~V1)+xlab("non-collapsed UMIs per cell")+ylab("unique UMIs per cell (trancripts)")
+## path with all the *.coutt.csv files that should be combined
+data_file = ""
+if (!is.na(args[1])) {
+	data_file <- args[1] 
+} else {
+	print("Data path param missing! Exit...\n")
+	quit(1)
+}
+
+print(data_file)
+
+sc_dat = read.csv(data_file,header = T,sep = "\t",stringsAsFactors = F)
+
+sc_dat$sample <- gsub(".*/","",sc_dat$sample)
+sc_dat$sample <- gsub(".coutc.csv$","",sc_dat$sample)
+
+png(file=paste(data_file,".reads_UMI_plot.png",sep=""),width=1500,height=1500)
+ggplot(dat=sc_dat,aes(x=(cell_reads),y=(cell_transcripts),color=sample))+geom_point(size=3,alpha=0.8) + 
+	facet_wrap(~sample,ncol=4)+
+	xlab("reads on feature per cell")+
+	ylab("unique UMIs per cell (trancripts)")+
+	theme(strip.text.x = element_text(size = 12, colour = "black",face="bold"))
+dev.off()
+
+## input:
+## sample                cell  cell_reads  sample2               cell2  cell_transcripts
+## B6_11wks_1.coutc.csv  1     40809       B6_11wks_1.coutb.csv  1      8716
+## B6_11wks_1.coutc.csv  2     17736       B6_11wks_1.coutb.csv  2      4615
+
+sc_dat <- sc_dat %>% group_by(sample) %>% mutate(sample_reads = sum(cell_reads)) %>% ungroup() %>% 
+		mutate(cRPM=cell_reads/sample_reads*1000000, cTPM=cell_transcripts/sample_reads*1000000) %>% 
+		mutate(cRPM_zscore = (log2(cRPM+5)-mean(log2(cRPM+5),trim=0.01))/sd(log2(cRPM+5)),
+			   cTPM_zscore = (log2(cTPM+5)-mean(log2(cTPM+5),trim=0.01))/sd(log2(cTPM+5)),
+			   y=-((cell-1)%%16),x=((cell-1)%/%16)+1)
+
+num_samples=length(levels(factor(sc_dat$sample)))
+print(num_samples)
+height=1000;
+
+if (num_samples>4){
+	height <- height + (num_samples-4)*50;
+}
+
+print(height)
+
+png(file=paste(data_file,".plate_cTPM.png",sep=""),width=1000,height=height)
+
+ggplot(sc_dat,aes(x=x,y=y,fill=cTPM_zscore))+ 
+		geom_tile() + 
+		facet_wrap(~sample,ncol = 4,scales = "free") + 
+		scale_fill_gradient2(low="red",mid="blue",limits=c(-4.5,2.5),high="cyan" ) + 
+		coord_fixed() + 
+		theme_minimal() +
+		theme(strip.text.x = element_text(size = 18, colour = "black",face="bold"))
+		
+dev.off()
+
+png(file=paste(data_file,".plate_cRPM.png",sep=""),width=1000,height=height)
+
+ggplot(sc_dat,aes(x=x,y=y,fill=cRPM_zscore))+ 
+		geom_tile() + 
+		facet_wrap(~sample,ncol = 4,scales = "free") + 
+		scale_fill_gradient2(low="red",mid="blue",limits=c(-4.5,2.5),high="cyan" ) + 
+		coord_fixed() + 
+		theme_minimal() +
+		theme(strip.text.x = element_text(size = 18, colour = "black",face="bold"))
+
+dev.off()
+
+
+png(file=paste(data_file,".plate_abs_transcripts.png",sep=""),width=1000,height=height)
+
+ggplot(sc_dat,aes(x=x,y=y,fill=cell_transcripts))+ 
+		geom_tile() + 
+		facet_wrap(~sample,ncol = 4,scales = "free") + 
+		#scale_fill_gradient2(low="red",mid="blue",high="cyan",limits=c(min(sc_dat$cell_transcripts),max(sc_dat$cell_transcripts)),midpoint=mean(sc_dat$cell_transcripts,trim=0.05)) + 
+		scale_fill_gradientn(colors=c("red","blue","cyan"),
+				values=rescale(c(0,3000,max(sc_dat$cell_transcripts))),
+				limits=c(0,max(sc_dat$cell_transcripts)),space = "Lab") +
+		coord_fixed() + 
+		theme_minimal() + 
+		theme(	strip.text.x = element_text(size = 17, colour = "white",face="bold"),
+				axis.text = element_text(size = 10, colour = "grey"),
+				axis.ticks.y = element_blank(),
+				plot.background = element_rect(fill = "black"),
+				panel.grid = element_blank() )
+
+dev.off()
+
--- workflows/scRNAseq/scRNAseq_QC_metrics.sh
+++ workflows/scRNAseq/scRNAseq_QC_metrics.sh
@@ -6,7 +6,7 @@ dir=$(realpath $dir)
 
 mkdir -p $dir/Results/QC
 
-for i in  $dir/Counts/*cout{b,c}.csv; do 
+for i in  $dir/Counts/*11wks_1*cout{b,c}.csv; do 
  out=$(echo $i | sed 's/.*\///'); 
 	echo $out 1>&2;
  cat $i | awk -v file=$out '{if (NR==1) {next;}; for (i=2;i<=NF;i++) COUNTS[i-1]+=$i;} \
@@ -14,9 +14,11 @@ for i in  $dir/Counts/*cout{b,c}.csv; do
       for (i=offset;i<offset+96;i++) {OFS="\t";print file,i,COUNTS[i];sum+=COUNTS[i];}}' >$dir/Results/QC/$out.cellsum;
 done
 
-paste <(cat $dir/Results/QC/*coutc*.cellsum) <(cat $dir/Results/QC/*coutb*.cellsum) > $dir/Results/all_samples.cellsum_coutc_countb.tsv
+## with header!
+paste <(echo -e "sample\tcell\tcell_reads"; cat $dir/Results/QC/*coutc*.cellsum) <(echo -e "sample2\tcell2\tcell_transcripts"; cat $dir/Results/QC/*coutb*.cellsum) > $dir/Results/all_samples.cellsum_coutc_countb.tsv
 
-cat  $dir/Results/all_samples.cellsum_coutc_countb.tsv | awk '{if (NR==1) last=$1; \
+## summary per sample/lib
+cat  $dir/Results/all_samples.cellsum_coutc_countb.tsv | awk '{if (NR<=2) last=$1; \
    if ($1==last) {sum1+=$3; sum2+=$6;} else {print last,sum1,sum2,sum2/sum1,sum1/96,sum2/96; sum1=0; sum2=0}; last=$1} \
    END{print last,sum1,sum2,sum2/sum1,sum1/96,sum2/96;}'
 
