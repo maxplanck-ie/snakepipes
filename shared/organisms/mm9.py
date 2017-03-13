674c45f1c797123d0f77f0359f3cb8f5866827ae
fkilpert <fkilpert@gmail.com>
2017-03-13 15:56:42 +0100

    Added STAR index

--- shared/organisms/mm9.py
+++ shared/organisms/mm9.py
@@ -5,6 +5,7 @@ genome_2bit = "/data/repository/organisms/GRCm37_ensembl/genome_fasta/genome.2bi
 bowtie2_index = "/data/repository/organisms/GRCm37_ensembl/BowtieIndex/genome"
 hisat2_index = "/data/repository/organisms/GRCm37_ensembl/HISAT2Index/genome"
 known_splicesites = "/data/repository/organisms/GRCm37_ensembl/gencode/m1/HISAT2/splice_sites.txt"
+star_index = "/data/repository/organisms/GRCm37_ensembl/STARIndex/"
 genes_bed = "/data/repository/organisms/GRCm37_ensembl/gencode/m1/genes.bed"
 genes_gtf = "/data/repository/organisms/GRCm37_ensembl/gencode/m1/genes.gtf"
 blacklist_bed = "/data/repository/organisms/GRCm37_ensembl/ENCODE/DacMapabilityConsensusExcludable.bed"
