fca992b2e33a89628198d0cf3d0d231a01b52b13
Michael Rauer <rauer@pc306.immunbio.mpg.de>
2017-05-10 16:51:31 +0200

    Automated t2g mapping from GTF file
    Yet to be tested, whether integration works correctly

--- shared/tools/extractTx2GeneMapping.sh
+++ shared/tools/extractTx2GeneMapping.sh
@@ -1,14 +1,11 @@
 #! /usr/bin/env bash
 
 GTF=$1
-OUT=$2
+OUTDIR=$2
 
-T2G=$OUT/$(basename ${GTF%%".gtf"}".t2g")
-GTF=/data/repository/organisms/GRCm38_ensembl/gencode/m9/genes.gtf
+T2G=$OUTDIR/$(basename ${GTF%%".gtf"}".t2g")
+# GTF=/data/repository/organisms/GRCm38_ensembl/gencode/m9/genes.gtf
 cat $GTF | grep -v '^##' | head -n2000 | awk '{ if ( $3 == "transcript" ) {print ($0) }}' | cut -f 9 | awk -F ";" 'BEGIN {OFS=";"}{  print ($2,$1,$5) } ' > tmp
-# cut -d ';' -f 2,1,5 > tmp
-
-# cat $GTF | grep -v '^##' | awk '{ if ( $3 == "transcript" ) {print ($0) }}' | cut -f 9 | cut -d ';' -f 2,1,5 > tmp
 
 for key in gene_id transcript_id gene_name "\"" " ";do  sed -i -e "s/$key//g" tmp ;done;
 
