f890ffb25c182e299618ec04064ef01bd3b59623
Michael Rauer <rauer@pc305.immunbio.mpg.de>
2017-05-30 15:44:24 +0200

    Added m13 annotation
    Annotation files are updated to gencode m13. Murine reference GRCm38
    remains.

---
+++ shared/organisms/m13.py
@@ -0,0 +1,11 @@
+genome_size = 2652783500
+genome_fasta = "/data/repository/organisms/GRCm38_ensembl/genome_fasta/genome.fa"
+genome_index = "/data/repository/organisms/GRCm38_ensembl/genome_fasta/genome.fa.fai"
+genome_2bit = "/data/repository/organisms/GRCm38_ensembl/genome_fasta/genome.2bit"
+bowtie2_index = "/data/repository/organisms/GRCm38_ensembl/BowtieIndex/genome"
+hisat2_index = "/data/repository/organisms/GRCm38_ensembl/HISAT2Index/genome"
+known_splicesites = "/data/repository/organisms/GRCm38_ensembl/gencode/m13/HISAT2/splice_sites.txt"
+star_index = "/data/repository/organisms/GRCm38_ensembl/STARIndex/"
+genes_bed = "/data/repository/organisms/GRCm38_ensembl/gencode/m13/genes.bed"
+genes_gtf = "/data/repository/organisms/GRCm38_ensembl/gencode/m13/genes.gtf"
+blacklist_bed = "/data/repository/organisms/GRCm38_ensembl/DKFZ/GRCm38_General_readAttractingRegions.bed"
