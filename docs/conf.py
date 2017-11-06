2dfe568e9cf5de9e0545534301fe0d7de34ea9ab
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2017-11-06 11:08:46 +0100

    trying again

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import shlex
 import mock
 
 # MOCK_MODULES = ['numpy', 'numpy.ma', 'scipy', 'pyBigWig']
-MOCK_MODULES = ['pyBigWig', 'py2bit']
+MOCK_MODULES = ['yaml', 're', 'inspect']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
