9cbb7b0df75d7cf7f55b6dc02da0e6c69a839065
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2018-04-20 11:15:16 +0200

    fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 ## at this point the script simply links the workflows in the main dir, but this is
 ## just a placeholder to make the point that the installation is coming in future
 
-for workflows in ChIP-seq RNA-seq HiC DNA-mapping ATAC-seq
+for workflow in ChIP-seq RNA-seq HiC DNA-mapping ATAC-seq
 do ln -s workflows/${workflow}/${workflow} .
 done
 # seperate link for scRNAseq
