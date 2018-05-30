8b66b83cc5715d519e712ed38f2d50b9aea17435
Katarzyna Sikora <sikora@pc329.immunbio.mpg.de>
2018-05-30 09:01:37 +0200

    fapos.py again

--- shared/tools/fapos.py
+++ shared/tools/fapos.py
@@ -21,42 +21,46 @@ def mod_fapos(sysargv):
     import argparse
     import re
     import datetime
-    def nicetime(): return datetime.datetime.now().strftime("[fapos %Y-%m-%d %H:%M:%S]")
 
+    def nicetime(): 
+        return datetime.datetime.now().strftime("[fapos %Y-%m-%d %H:%M:%S]")
 
     #######################################
     # arguments, filehandles
 	
     parser = argparse.ArgumentParser(prog="methylCtools fapos", version="0.9.4", description="creates cytosine position index for defined context")
     parser.add_argument("-s", "--silent", dest="qf", action="store_false", help="do not show status messages")
-	
+
     groupinput = parser.add_argument_group("input files, required")
     groupinput.add_argument("inREF", metavar="ref.fa", action="store", default=False, help="reference sequence (fasta format)")
-	
+
     groupoutput = parser.add_argument_group("output files, will be created")
     groupoutput.add_argument("outPOS", metavar="ref.pos", action="store", default=False, help="cytosine position index (tab format, \"-\" for stdout)")
-	
+
     groupcontext = parser.add_mutually_exclusive_group()
     groupcontext.add_argument("-1", dest="c1", action="store_true", help="output positions for C context")
     groupcontext.add_argument("-2", dest="c2", action="store_true", help=".. CG context (default)")
     groupcontext.add_argument("-3", dest="c3", action="store_true", help=".. CG/CH context")
     groupcontext.add_argument("-4", dest="c4", action="store_true", help=".. CG/CHG/CHH context")
-	
+
     args = parser.parse_args(sysargv)
-	
+
     try:
         inFA = open(args.inREF, "r")
-        if args.outPOS == "-": outPOS = sys.stdout
-        else: outPOS = open(args.outPOS, "w")
-		
+        if args.outPOS == "-": 
+            outPOS = sys.stdout
+        else: 
+            outPOS = open(args.outPOS, "w")
+
     except IOError as strerror:
         sys.exit("methylCtools fapos: error: %s" % strerror)
 
     t = [args.c1, args.c2, args.c3, args.c4]
-    if not sum(t): t[1] = True
+    if not sum(t): 
+        t[1] = True
 
-    if args.qf: sys.stderr.write("%s command: %s\n" % (nicetime(), " ".join(sys.argv)))
-
+    if args.qf: 
+        sys.stderr.write("%s command: %s\n" % (nicetime(), " ".join(sys.argv)))
 
     #######################################
     # functions
@@ -94,15 +98,19 @@ def mod_fapos(sysargv):
                         outPOS.write("%s\t%i\t%s\t%s\t%s\n" % (cname, i, "-", "CHH", rcomp(seq[(i-2):(i+1)])))		
 	
 
-	#######################################
-	# main
-							
-    if t[0]: tc = "C"
-    elif t[1]: tc = "CG"
-    elif t[2]: tc = "CG/CH"
-    else: tc = "CG/CHG/CHH"
-    if args.qf: sys.stderr.write("%s start: generating position index for %s context\n" % (nicetime(), tc))
-	
+    #######################################
+    # main
+    if t[0]:
+        tc = "C"
+    elif t[1]:
+        tc = "CG"
+    elif t[2]:
+        tc = "CG/CH"
+    else:
+        tc = "CG/CHG/CHH"
+    if args.qf:
+        sys.stderr.write("%s start: generating position index for %s context\n" % (nicetime(), tc))
+
     cname = False
     p = re.compile("[C,G]", re.IGNORECASE)
     for line in inFA:
@@ -113,22 +121,22 @@ def mod_fapos(sysargv):
                     pp(m.start(), seqlen)
             cname = line.rstrip()[1:]
             seq = ""
-            if args.qf: sys.stderr.write("%s status: processing %s\n" % (nicetime(), cname))
+            if args.qf:
+                sys.stderr.write("%s status: processing %s\n" % (nicetime(), cname))
         else:
             seq += line.rstrip()
-	
+ 
     seqlen = len(seq)
     for m in p.finditer(seq):
         pp(m.start(), seqlen)
-
-
+ 
     #######################################
     # end
-
     inFA.close()
     outPOS.close()
 
-    if args.qf: sys.stderr.write("%s end: position index generated\n" % nicetime())
+    if args.qf:
+        sys.stderr.write("%s end: position index generated\n" % nicetime())
 
 if __name__ == "__main__":
     import sys
