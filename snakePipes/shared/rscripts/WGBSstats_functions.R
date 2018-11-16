4e93bf8758919ae62d650181444ffbad2ba096c2
Katarzyna Sikora <sikora@pc329.immunbio.mpg.de>
2018-11-16 15:43:57 +0100

    testing one WGBS stats report template

--- snakePipes/shared/rscripts/WGBSstats_functions.R
+++ snakePipes/shared/rscripts/WGBSstats_functions.R
@@ -7,5 +7,17 @@ print_sessionInfo<-function(mytext){
     sink()
     }
 
+get_nrow_topTable<-function(resdir,statscat){
+    if(statscat %in% "single_CpGs"){
+        load(file.path(resdir,"singleCpG.RData"))
+        nrtt<-nrow(tT_filt)
+    } else if (statscat %in% "metilene_DMRs"){
+        nrtt<-system(paste0('wc -l ',file.path(resdir,"singleCpG.metilene.limma_filtered.bed")),intern=TRUE)
+    } else if (statscat %in% "user_intervals"){
+        nrtt<-unlist(lapply(dir(resdir,pattern="*.tT_filt.txt",full.names=TRUE),function(X)system(paste0('wc -l ',X),intern=TRUE)))
+    }
+    return(nrtt)
+}
+
 
 
