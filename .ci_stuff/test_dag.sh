e5c29d27faeb64428cf2e97d264d2f0b85a3a78b
dpryan79 <dpryan79@gmail.com>
2019-04-04 16:43:56 +0200

    Test DNA and ChIP-seq workflows, remove --BAMPE option which does nothing

--- .ci_stuff/test_dag.sh
+++ .ci_stuff/test_dag.sh
@@ -1,31 +1,70 @@
 #!/bin/bash
-export PATH="$HOME/miniconda/bin:$PATH"
-hash -r
-python -m pip install --no-deps --ignore-installed .
+set -e
+#export PATH="$HOME/miniconda/bin:$PATH"
+#hash -r
+#python -m pip install --no-deps --ignore-installed .
 
 
-# Make some fake data
-mkdir PE_input
+# Needed by DNA, HiC, RNA-seq, WGBS and scRNA-seq workflows
+mkdir -p PE_input
 touch PE_input/sample1_R1.fastq.gz PE_input/sample1_R2.fastq.gz \
       PE_input/sample2_R1.fastq.gz PE_input/sample2_R2.fastq.gz \
       PE_input/sample3_R1.fastq.gz PE_input/sample3_R2.fastq.gz \
       PE_input/sample4_R1.fastq.gz PE_input/sample4_R2.fastq.gz \
       PE_input/sample5_R1.fastq.gz PE_input/sample5_R2.fastq.gz \
       PE_input/sample6_R1.fastq.gz PE_input/sample6_R2.fastq.gz
-mkdir SE_input
+mkdir -p SE_input
 touch SE_input/sample1_R1.fastq.gz \
       SE_input/sample2_R1.fastq.gz \
       SE_input/sample3_R1.fastq.gz \
       SE_input/sample4_R1.fastq.gz \
       SE_input/sample5_R1.fastq.gz \
       SE_input/sample6_R1.fastq.gz
+# Needed by ChIP and ATAC workflows
+mkdir -p BAM_input/deepTools_qc/bamPEFragmentSize BAM_input/filtered_bam BAM_input/Sambamba
+touch BAM_input/sample1.bam \
+      BAM_input/sample2.bam \
+      BAM_input/sample3.bam \
+      BAM_input/sample4.bam \
+      BAM_input/sample5.bam \
+      BAM_input/sample6.bam \
+      BAM_input/filtered_bam/sample1.filtered.bam \
+      BAM_input/filtered_bam/sample2.filtered.bam \
+      BAM_input/filtered_bam/sample3.filtered.bam \
+      BAM_input/filtered_bam/sample4.filtered.bam \
+      BAM_input/filtered_bam/sample5.filtered.bam \
+      BAM_input/filtered_bam/sample6.filtered.bam \
+      BAM_input/filtered_bam/sample1.filtered.bam.bai \
+      BAM_input/filtered_bam/sample2.filtered.bam.bai \
+      BAM_input/filtered_bam/sample3.filtered.bam.bai \
+      BAM_input/filtered_bam/sample4.filtered.bam.bai \
+      BAM_input/filtered_bam/sample5.filtered.bam.bai \
+      BAM_input/filtered_bam/sample6.filtered.bam.bai \
+      BAM_input/Sambamba/sample1.markdup.txt \
+      BAM_input/Sambamba/sample2.markdup.txt \
+      BAM_input/Sambamba/sample3.markdup.txt \
+      BAM_input/Sambamba/sample4.markdup.txt \
+      BAM_input/Sambamba/sample5.markdup.txt \
+      BAM_input/Sambamba/sample6.markdup.txt \
+      BAM_input/deepTools_qc/bamPEFragmentSize/fragmentSize.metric.tsv
+mkdir -p output
 
 # DNA mapping
-rm -rf output && mkdir output
-DNA-mapping -i PE_input -o output mm10 --snakemake_options " --dryrun"
-rm -rf output && mkdir output
-DNA-mapping -i PE_input -o output mm10 --snakemake_options " --dryrun" --trim --mapq 20 --dedup --properpairs
-rm -rf output && mkdir output
-DNA-mapping -i SE_input -o output mm10 --snakemake_options " --dryrun"
-rm -rf output && mkdir output
-DNA-mapping -i SE_input -o output mm10 --snakemake_options " --dryrun" --trim --mapq 20 --dedup --properpairs
+WC=`DNA-mapping -i PE_input -o output mm10 --snakemake_options " --dryrun" | tee /dev/stderr | wc -l`
+if [[ $WC -ne 734 || ${PIPESTATUS[0]} -ne 0 ]]; then exit 1 ; fi
+WC=`DNA-mapping -i PE_input -o output mm10 --snakemake_options " --dryrun" --trim --mapq 20 --dedup --properpairs | tee /dev/stderr | wc -l`
+if [[ $WC -ne 936 || ${PIPESTATUS[0]} -ne 0 ]]; then exit 1 ; fi
+WC=`DNA-mapping -i SE_input -o output mm10 --snakemake_options " --dryrun" | tee /dev/stderr | wc -l`
+if [[ $WC -ne 684 || ${PIPESTATUS[0]} -ne 0 ]]; then exit 1 ; fi
+WC=`DNA-mapping -i SE_input -o output mm10 --snakemake_options " --dryrun" --trim --mapq 20 --dedup --properpairs | tee /dev/stderr | wc -l`
+if [[ $WC -ne 820 || ${PIPESTATUS[0]} -ne 0 ]]; then exit 1 ; fi
+
+# ChIP-seq
+WC=`ChIP-seq -d BAM_input --snakemake_options " --dryrun" mm10 .ci_stuff/ChIP.sample_config.yaml | tee /dev/stderr | wc -l`
+if [[ $WC -ne 268 || ${PIPESTATUS[0]} -ne 0 ]]; then exit 1 ; fi
+WC=`ChIP-seq -d BAM_input --snakemake_options " --dryrun" --single-end mm10 .ci_stuff/ChIP.sample_config.yaml | tee /dev/stderr | wc -l`
+if [[ $WC -ne 268 || ${PIPESTATUS[0]} -ne 0 ]]; then exit 1 ; fi
+WC=`ChIP-seq -d BAM_input --snakemake_options " --dryrun" --bigWigType log2ratio mm10 .ci_stuff/ChIP.sample_config.yaml | tee /dev/stderr | wc -l`
+if [[ $WC -ne 222 || ${PIPESTATUS[0]} -ne 0 ]]; then exit 1 ; fi
+
+rm -rf SE_input PE_input BAM_input output
