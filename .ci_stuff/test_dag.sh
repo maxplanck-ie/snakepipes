d35848b2e691b9c04109f6ed7e20eaf223b2e23b
dpryan79 <dpryan79@gmail.com>
2019-04-05 11:22:46 +0200

    use a custom organism

--- .ci_stuff/test_dag.sh
+++ .ci_stuff/test_dag.sh
@@ -49,65 +49,66 @@ touch BAM_input/sample1.bam \
       BAM_input/Sambamba/sample6.markdup.txt \
       BAM_input/deepTools_qc/bamPEFragmentSize/fragmentSize.metric.tsv
 mkdir -p output
+touch genes.gtf
 
 # DNA mapping
-WC=`DNA-mapping -i PE_input -o output --tempdir /tmp mm10 --snakemake_options " --dryrun" | tee /dev/stderr | wc -l`
+WC=`DNA-mapping -i PE_input -o output --tempdir /tmp .ci_stuff/organism.yaml --snakemake_options " --dryrun" | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 734 ]; then exit 1 ; fi
-WC=`DNA-mapping -i PE_input -o output --tempdir /tmp mm10 --snakemake_options " --dryrun" --trim --mapq 20 --dedup --properpairs | tee /dev/stderr | wc -l`
+WC=`DNA-mapping -i PE_input -o output --tempdir /tmp .ci_stuff/organism.yaml --snakemake_options " --dryrun" --trim --mapq 20 --dedup --properpairs | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 936 ]; then exit 1 ; fi
-WC=`DNA-mapping -i SE_input -o output --tempdir /tmp mm10 --snakemake_options " --dryrun" | tee /dev/stderr | wc -l`
+WC=`DNA-mapping -i SE_input -o output --tempdir /tmp .ci_stuff/organism.yaml --snakemake_options " --dryrun" | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 684 ]; then exit 1 ; fi
-WC=`DNA-mapping -i SE_input -o output --tempdir /tmp mm10 --snakemake_options " --dryrun" --trim --mapq 20 --dedup --properpairs | tee /dev/stderr | wc -l`
+WC=`DNA-mapping -i SE_input -o output --tempdir /tmp .ci_stuff/organism.yaml --snakemake_options " --dryrun" --trim --mapq 20 --dedup --properpairs | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 820 ]; then exit 1 ; fi
 
 # ChIP-seq
-WC=`ChIP-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" mm10 .ci_stuff/ChIP.sample_config.yaml | tee /dev/stderr | wc -l`
+WC=`ChIP-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" .ci_stuff/organism.yaml .ci_stuff/ChIP.sample_config.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 268 ]; then exit 1 ; fi
-WC=`ChIP-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" --single-end mm10 .ci_stuff/ChIP.sample_config.yaml | tee /dev/stderr | wc -l`
+WC=`ChIP-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" --single-end .ci_stuff/organism.yaml .ci_stuff/ChIP.sample_config.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 268 ]; then exit 1 ; fi
-WC=`ChIP-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" --bigWigType log2ratio mm10 .ci_stuff/ChIP.sample_config.yaml | tee /dev/stderr | wc -l`
+WC=`ChIP-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" --bigWigType log2ratio .ci_stuff/organism.yaml .ci_stuff/ChIP.sample_config.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 222 ]; then exit 1 ; fi
 
 # ATAC-seq
-WC=`ATAC-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" mm10 | tee /dev/stderr | wc -l`
+WC=`ATAC-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 336 ]; then exit 1 ; fi
-WC=`ATAC-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" --fragmentSize-cutoff 120 --qval 0.1 mm10 | tee /dev/stderr | wc -l`
+WC=`ATAC-seq -d BAM_input --tempdir /tmp --snakemake_options " --dryrun" --fragmentSize-cutoff 120 --qval 0.1 .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 336 ]; then exit 1 ; fi
 
 # RNA-seq
-WC=`RNA-seq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" mm10 | tee /dev/stderr | wc -l`
+WC=`RNA-seq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 889 ]; then exit 1 ; fi
-WC=`RNA-seq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" -m "alignment" mm10 | tee /dev/stderr | wc -l`
+WC=`RNA-seq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" -m "alignment" .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 578 ]; then exit 1 ; fi
-WC=`RNA-seq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" -m "alignment,deepTools_qc" --trim mm10 | tee /dev/stderr | wc -l`
+WC=`RNA-seq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" -m "alignment,deepTools_qc" --trim .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 1052 ]; then exit 1 ; fi
-WC=`RNA-seq -i SE_input -o output --tempdir /tmp --snakemake_options " --dryrun" mm10 | tee /dev/stderr | wc -l`
+WC=`RNA-seq -i SE_input -o output --tempdir /tmp --snakemake_options " --dryrun" .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 828 ]; then exit 1 ; fi
-WC=`RNA-seq -i SE_input -o output --tempdir /tmp --snakemake_options " --dryrun" -m "alignment" mm10 | tee /dev/stderr | wc -l`
+WC=`RNA-seq -i SE_input -o output --tempdir /tmp --snakemake_options " --dryrun" -m "alignment" .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 528 ]; then exit 1 ; fi
-WC=`RNA-seq -i SE_input -o output --tempdir /tmp --snakemake_options " --dryrun" -m "alignment,deepTools_qc" --trim mm10 | tee /dev/stderr | wc -l`
+WC=`RNA-seq -i SE_input -o output --tempdir /tmp --snakemake_options " --dryrun" -m "alignment,deepTools_qc" --trim .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 925 ]; then exit 1 ; fi
 
 # HiC - fails
-WC=`HiC -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" mm10 | tee /dev/stderr | wc -l`
+WC=`HiC -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 522 ]; then exit 1 ; fi
-WC=`HiC -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --trim mm10 | tee /dev/stderr | wc -l`
+WC=`HiC -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --trim .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 724 ]; then exit 1 ; fi
-WC=`HiC -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --enzyme DpnII mm10 | tee /dev/stderr | wc -l`
+WC=`HiC -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --enzyme DpnII .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 522 ]; then exit 1 ; fi
-WC=`HiC -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --noTAD mm10 | tee /dev/stderr | wc -l`
+WC=`HiC -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --noTAD .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 460 ]; then exit 1 ; fi
 
 # scRNA-seq
-WC=`scRNAseq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" mm10 | tee /dev/stderr | wc -l`
+WC=`scRNAseq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 1080 ]; then exit 1 ; fi
-WC=`scRNAseq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --skipRaceID --split_lib mm10 | tee /dev/stderr | wc -l`
+WC=`scRNAseq -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --skipRaceID --split_lib .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 1051 ]; then exit 1 ; fi
 
 # WGBS
-WC=`WGBS -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" mm10 | tee /dev/stderr | wc -l`
+WC=`WGBS -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 906 ]; then exit 1 ; fi
-WC=`WGBS -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --skipDOC --skipGCbias --nextera mm10 | tee /dev/stderr | wc -l`
+WC=`WGBS -i PE_input -o output --tempdir /tmp --snakemake_options " --dryrun" --skipDOC --skipGCbias --nextera .ci_stuff/organism.yaml | tee /dev/stderr | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 788 ]; then exit 1 ; fi
 
-rm -rf SE_input PE_input BAM_input output
+rm -rf SE_input PE_input BAM_input output genes.gtf
