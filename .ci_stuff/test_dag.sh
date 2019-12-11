18f25540a2ccf5cf862868d4c144faf1e4bea516
katsikora <sikora@pc332.immunbio.mpg.de>
2019-12-11 12:18:15 +0100

    updated test.dag

--- .ci_stuff/test_dag.sh
+++ .ci_stuff/test_dag.sh
@@ -119,7 +119,7 @@ if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 519 ]; then exit 1 ; fi
 
 # scRNA-seq
 WC=`scRNAseq -i PE_input -o output --snakemakeOptions " --dryrun --conda-prefix /tmp" .ci_stuff/organism.yaml | tee >(cat 1>&2) | grep -v "Conda environment" | wc -l`
-if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 1283 ]; then exit 1 ; fi
+if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 1385 ]; then exit 1 ; fi
 WC=`scRNAseq -i PE_input -o output --snakemakeOptions " --dryrun --conda-prefix /tmp" --skipRaceID --splitLib .ci_stuff/organism.yaml | tee >(cat 1>&2) | grep -v "Conda environment" | wc -l`
 if [ ${PIPESTATUS[0]} -ne 0 ] || [ $WC -ne 1254 ]; then exit 1 ; fi
 
