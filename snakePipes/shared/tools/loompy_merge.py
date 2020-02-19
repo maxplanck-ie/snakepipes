3fe733414b56029e535030d8b5edacd2892a8f63
katsikora <sikora@maximus.ie-freiburg.mpg.de>
2020-02-19 14:16:57 +0100

    more flake fixes

--- snakePipes/shared/tools/loompy_merge.py
+++ snakePipes/shared/tools/loompy_merge.py
@@ -3,8 +3,8 @@ import loompy
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument("-outf", "--output", dest = "outf", help = "output file", metavar = "FILE")
-parser.add_argument('input', nargs='+', help = "input folders")
+parser.add_argument("-outf", "--output", dest="outf", help="output file", metavar="FILE")
+parser.add_argument('input', nargs='+', help="input folders")
 
 args = parser.parse_args()
 
@@ -15,8 +15,8 @@ filelist = []
 for p in input:
     z = os.listdir(p)
     f = list(filter(lambda x: '.loom' in x, z))
-    ifi = os.path.join(p,f[0])
+    ifi = os.path.join(p, f[0])
     filelist.append(ifi)
 print(filelist)
 print(outf)
-loompy.combine(files = filelist, output_file = outf, key = "Accession")
+loompy.combine(files=filelist, output_file=outf, key="Accession")
