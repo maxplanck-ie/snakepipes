3bf4e206ae862c9b4bed158417e7c2c50011df6b
Leily Rabbani <rabbani@pc390.ie-freiburg.mpg.de>
2021-03-15 14:36:24 +0100

    flake

--- .azure-pipelines/testCreateEnv.py
+++ .azure-pipelines/testCreateEnv.py
@@ -5,9 +5,9 @@ import sys
 sys.path.append("..")
 from snakePipes import common_functions as cf
 keys = []
-index = len(cf.set_env_yamls().items())/10
+index = len(cf.set_env_yamls().items()) / 10
 index = round(index)
 for i in range(index):
     keys.append(list(cf.set_env_yamls())[i])
 envs_to_test = " ".join(keys)
-sp.check_output( "snakePipes createEnvs --only {} --force".format(envs_to_test), shell =True)
+sp.check_output("snakePipes createEnvs --only {} --force".format(envs_to_test), shell = True)
