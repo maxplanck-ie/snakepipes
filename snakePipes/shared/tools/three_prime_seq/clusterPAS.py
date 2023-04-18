f032cb80f648388cd979c433205c5b0d2627ee44
David Koppstein <koppstein@ie-freiburg.mpg.de>
2023-04-18 16:41:16 +0200

    ignore three_prime_seq old scripts

--- snakePipes/shared/tools/three_prime_seq/clusterPAS.py
+++ snakePipes/shared/tools/three_prime_seq/clusterPAS.py
@@ -31,22 +31,22 @@ def cluster_pas(args):
                     #cluster start, cluster end, max count, gene, annotation, summit
                     clusters[gene][pas] = [pas, pas + 1, count, gene, annotation, pas, strand, chrom]
                 else:
-                     newCluster = True
-                     for c in clusters[gene]:
-                         if pas > clusters[gene][c][0] - window and pas < clusters[gene][c][1] + window:
-                              clusters[gene][c][0] = min(pas, clusters[gene][c][0]) 
-                              clusters[gene][c][1] = max(pas + 1, clusters[gene][c][1])
-                              clusters[gene][c][2] += count
-                              if count > clusters[gene][c][2]:
-                                  #clusters[gene][c][2] = count
-                                  clusters[gene][c][4] = annotation
-                                  clusters[gene][c][5] = pas
-                              newCluster = False
-                              break
+                    newCluster = True
+                    for c in clusters[gene]:
+                        if pas > clusters[gene][c][0] - window and pas < clusters[gene][c][1] + window:
+                            clusters[gene][c][0] = min(pas, clusters[gene][c][0]) 
+                            clusters[gene][c][1] = max(pas + 1, clusters[gene][c][1])
+                            clusters[gene][c][2] += count
+                            if count > clusters[gene][c][2]:
+                                #clusters[gene][c][2] = count
+                                clusters[gene][c][4] = annotation
+                                clusters[gene][c][5] = pas
+                            newCluster = False
+                            break
 
-                     #not close to any existing cluster => new cluster
-                     if newCluster:
-                         clusters[gene][pas] = [pas, pas + 1, count, gene, annotation, pas, strand, chrom]            
+                    #not close to any existing cluster => new cluster
+                    if newCluster:
+                        clusters[gene][pas] = [pas, pas + 1, count, gene, annotation, pas, strand, chrom]            
 
             line = In.readline()
 
--- snakePipes/shared/tools/three_prime_seq/dedup_clusterPAS.py
+++ snakePipes/shared/tools/three_prime_seq/dedup_clusterPAS.py
@@ -5,7 +5,7 @@ import sys
 
 """
 Read in post-processed output of clusterPAS from STDIN, add header,
-deduplicate the Gene column (4th), write to STDOUT. 
+deduplicate the Gene column (4th), write to STDOUT.
 """
 
 
@@ -22,15 +22,21 @@ HEADERS = [
 
 
 def dedup(sub_df):
-    new_ids = ["_".join([str(gene), str(i)]) for (i, gene) in enumerate(sub_df['Gene'])]
+    new_ids = ["_".join([str(gene), str(i)]) for
+               (i, gene) in enumerate(sub_df['Gene'])]
     sub_df['Gene'] = new_ids
     return sub_df
-    
+
 
 def main():
     df = pd.read_table(sys.stdin, index_col=None, header=0)
-    df = df.groupby("Gene", group_keys=False).apply(dedup).reset_index(drop=True)
+    df = (
+        df.groupby("Gene", group_keys=False)
+        .apply(dedup)
+        .reset_index(drop=True)
+    )
     df.to_csv(sys.stdout, sep="\t", index=False, header=False)
+
 
 if __name__ == "__main__":
     main()
