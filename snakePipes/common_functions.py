5b0be611ab2beeb5d3e476334008f494dfdc3315
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2023-08-03 12:27:28 +0200

    more flake fixes

--- snakePipes/common_functions.py
+++ snakePipes/common_functions.py
@@ -820,11 +820,12 @@ def writeTools(usedEnvs, wdir, workflowName, maindir):
                     f.write(line)
     f.close()
 
+
 def copySampleSheet(sampleSheet, wdir):
     if os.path.isfile(sampleSheet) and os.path.exists(wdir):
         bname = os.path.basename(sampleSheet)
         try:
-            shutil.copy(sampleSheet, os.path.join(wdir,bname))
+            shutil.copy(sampleSheet, os.path.join(wdir, bname))
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
             raise
