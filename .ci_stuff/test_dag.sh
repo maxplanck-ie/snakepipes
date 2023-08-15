40ca59a05363fbe7d456b1883169cf377a5291f2
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2023-08-15 13:52:52 +0200

    fix for test dag

--- .ci_stuff/test_dag.sh
+++ .ci_stuff/test_dag.sh
@@ -113,6 +113,7 @@ touch allelic_input/file.vcf.gz allelic_input/snpfile.txt
 cp .ci_stuff/genome.fa .ci_stuff/genome.fa.fai /tmp/
 mkdir -p /tmp/SalmonIndex
 touch /tmp/SalmonIndex/decoys.txt
+touch /tmp/annotation/cDNA_introns.joint.t2g
 
 # Ensure an empty snakePipes config doesn't muck anything up
 snakePipes config
