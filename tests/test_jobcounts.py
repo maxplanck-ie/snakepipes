3aed8cb634007c7113c167ce03a32ca906bbf710
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2023-12-21 13:18:07 +0100

    pytest and news

--- tests/test_jobcounts.py
+++ tests/test_jobcounts.py
@@ -166,12 +166,12 @@ def createTestData(fp, samples=9) -> None:
 
     chip_dict ={
         'chip_dict': {
-            'sample1': {'control': , 'broad': 'False'},
-            'sample2': {'control': , 'broad': 'False'},
-            'sample3': {'control': , 'broad': 'False'},
-            'sample4': {'control': , 'broad': 'False'},
-            'sample5': {'control': , 'broad': 'False'},
-            'sample6': {'control': , 'broad': 'False'}
+            'sample1': {'control': '', 'broad': 'False'},
+            'sample2': {'control': '', 'broad': 'False'},
+            'sample3': {'control': '', 'broad': 'False'},
+            'sample4': {'control': '', 'broad': 'False'},
+            'sample5': {'control': '', 'broad': 'False'},
+            'sample6': {'control': '', 'broad': 'False'}
         }
     }
     with open(fp / 'chipdict_noControl.yaml', 'w') as f:
