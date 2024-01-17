e602a7b2fd3dfcf67f524279f55de4e7fc7e0fae
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2024-01-17 13:00:48 +0100

    does this work

--- tests/test_jobcounts.py
+++ tests/test_jobcounts.py
@@ -537,6 +537,7 @@ class TestChIPseq:
         assert parseSpOut(_p) == 74
     def test_seacr(self, ifs):
         ci = [
+            "cat", ifs / 'chipdict.yaml;',
             "ChIP-seq",
             '-d',
             ifs / 'bam_input',
