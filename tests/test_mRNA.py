8127ab509ed83b6e458bfd2a9fa21a82d7914847
WardDeb <ward@deboutte.be>
2024-06-25 16:59:56 +0200

    init true mRNA run test

--- tests/test_mRNA.py
+++ tests/test_mRNA.py
@@ -1,92 +1,74 @@
-import os.path
 from pathlib import Path
 import subprocess as sp
-import pytest
 from ruamel.yaml import YAML
+import shutil
+import gzip
+import pytest
 
+SMKOPTS = " --dryrun --conda-prefix /tmp -q "
 
+def extract_gz(i, o):
+  '''
+  extracts i into file o
+  '''
+  with gzip.open(i, 'rb') as f:
+    with open(o, 'wb') as of:
+      shutil.copyfileobj(f, of)
 
-#  for the functionality of tests/test_mrnaseq.py
-# * 		function that creates the STAR index from fasta and gtf (note my previous message)
-# * 		Create organism yaml with STAR index you just created
-# * 		Create a custom config file which sets conda path to be empty
-# * 		Have envs created related to mRNA-seq workflow (createIndices functionality)
-# * 		implement the actual test
+def createTestData(fp):
+    '''
+    Sets up fasta, gtf and organism yaml in a factory (fp)
+    '''
+    # Provide cluster yaml
+    clsyml = Path('tests') / ' data' / 'cluster_config.yaml'
+    shutil.copyfile(clsyml, fp / 'cconf.yaml')
+    # uncompress fna into tmp factory
+    fnagz = Path('tests') / 'data' / 'genomes' / 'genome_chr17.fa.gz'
+    faout = fp / 'genome.fa'
+    extract_gz(fnagz, faout)
+    # uncompress gtf into tmp factory
+    gtfgz = Path('tests') / 'data' / 'genomes' / 'genes_chr17.gtf.gz'
+    gtfout = fp / 'genes.gtf'
+    extract_gz(gtfgz, gtfout)
+    # Path to STAR index (created in action)
+    STARpath = Path('tests') / 'data' / 'mRNA_STAR'
+    orgyaml = {
+      "genome_size": 94987271 , #we can also extract genome size from STARindex output
+      "genome_fasta": faout.as_posix(),
+      "star_index": STARpath.as_posix(),
+      "genes_gtf" : gtfout.as_posix(),
+      "extended_coding_regions_gtf" : "",
+      "blacklist_bed": "",
+      "ignoreForNormalization": "" 
+    }
+    # set up yaml
+    yaml = YAML()
+    yaml.boolean_representation = ['False', 'True']
+    with open(fp / 'org.yaml', 'w') as of:
+        yaml.dump(orgyaml, of)
 
+@pytest.fixture(scope='session')
+def ifs(tmp_path_factory):
+  fp = tmp_path_factory.mktemp("data")
+  createTestData(fp)
+  return fp
 
+class TestmRNAseq:
+    def test_mrna(self, ifs):
+      org = ifs / 'org.yaml'
+      clusterconfig = ifs / 'cconf.yaml'
+      sp.run(
+        [
+          'mRNA-seq',
+          '-i',
+          Path('tests') / 'data' / 'mRNA'
+          '-o',
+          'test_mrna',
+          '--clusterConfig',
+          clusterconfig,
+          org
+        ]
+      )
+      assert Path('test_mrna/mRNA-seq_snakePipes.done').is_file == True
 
-genomedir = Path(__file__).parents[0]/ 'data' / 'genomes'
-datadir = Path(__file__).parents[0] / 'data' / 'mRNA'
-gtf = genomedir/'genes_chr17.gtf'
-fa = genomedir/'genome_chr17.fa'
-STARdir = genomedir / 'STAR_index'
-
-def STAR_ix():
-  assert gtf.exists(), f"GTF file does not exist at {gtf}"
-  assert fa.exists(), f"FASTA file does not exist at {fa}"
-
-  sp.run(['STAR' , '--runThreadN' , '8', '--runMode', 'genomeGenerate' , 
-          '--genomeDir' , str(STARdir), '--genomeFastaFiles' , str(fa), 
-          '--sjdbGTFfile',str(gtf), '--sjdbOverhang', '100', '--genomeSAindexNbases' , '12'], check=True)
-  
-  return STARdir.as_posix()
       
-
-def createYaml(yaml_file):
-  yamlPath  = datadir / yaml_file
-  _yaml_ = YAML()
-
-  data = {
-     "genome_size": 94987271 , #we can also extract genome size from STARindex output
-     "genome_fasta": "data/manke/group/schmidth/tests/data/genomes/genome_chr17.fa",
-     "star_index": "STARdir",
-     "genes_gtf" : "data/manke/group/schmidth/tests/data/genomes/genes_chr17.gtf", 'path(__file_) relative' as_posix
-     "extended_coding_regions_gtf" : "",
-     "blacklist_bed": "",
-     "ignoreForNormalization": "" 
-     }
-  
-  with open(yamlPath, 'w') as file: 
-     _yaml_.dump(data, file)
-
-  return yamlPath.as_posix()
-
-
-
-def config_conda(_config):
-  config_path = Path(__file__).parents[0] / 'data' / 'env' / _config
-  config_data = { "conda_path": "" }
-
-  with open(config_path, "w") as config_file:
-    _yaml = YAML()
-    _yaml.dump(config_data, config_file)
-  
-  return config_path.as_posix()
-
- 
-
-# def create_indices():
-#   sp.run(['createIndices', '-o', str(genomedir), 
-#           '--genomeURL', str(fa),  
-#           '--gtfURL', str(gtf), 
-#           '--userYAML',  
-#           'mm10_M19_chr17'], 
-#             check = True)
-
-def test_mrna():
-    yaml_file = 'mm10_chr17.yaml'
-    yaml_path = createYaml(yaml_file)
-    STAR_index = STAR_ix()
-    _config = 'conda.yaml'
-    config= config_conda(_config)
-    indices = create_indices()  
-
-    assert Path(STAR_index).exists(), "STAR index directory does not exist"
-    assert Path(yaml_path).exists(), "Organism YAML file does not exist"
-    assert Path(config).exists(), "Custom config file does not exist"
-
-    return yaml_path, STAR_index, config, indices
-
-test_mrna()
-
-
