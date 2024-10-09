37f8434af803b28d87cd6d868b8fcac8767b9878
WardDeb <ward@deboutte.be>
2024-10-09 18:17:14 +0200

    lint conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -150,7 +150,7 @@ todo_include_todos = True
 
 # import them both locally and on rtd
 html_theme = 'sphinx_rtd_theme'  # 'alabaster' 'sphinx_rtd_theme'
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()] # deprecated 
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
