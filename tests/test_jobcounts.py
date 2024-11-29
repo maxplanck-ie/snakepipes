7fdd87b40cf26d33f6f26187ac2a0e24bc33bbab
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2024-11-29 13:32:37 +0100

    fix

--- tests/test_jobcounts.py
+++ tests/test_jobcounts.py
@@ -1307,7 +1307,7 @@ class TestmRNAseq:
         print(' '.join([str(i) for i in ci]))
         _p = sp.run(ci, capture_output=True, text=True)
         assert _p.returncode == 0
-        assert parseSpOut(_p) == 177
+        assert parseSpOut(_p) == 186
     def test_multicomp(self, ifs):
         ci = [
             "mRNAseq",
