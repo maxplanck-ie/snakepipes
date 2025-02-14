8861f64e1efc6f0385150c684726538f38b48f06
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2025-02-14 13:41:59 +0100

    chipqc

--- snakePipes/shared/rscripts/chipqc.R
+++ snakePipes/shared/rscripts/chipqc.R
@@ -27,12 +27,13 @@ markv<-names(b)
 #first implementation: ignore sample sheet and condition and replicates
 #if sample sheet is a file path: get condition and replicate information
 #the check that the sample sheet file exists is taken care of by the python wrapper
-if (sampleSheet == "" || is.null(sampleSheet)):
-    sampledat<-data.frame("SampleID"=samples,"Condition"=rep("All",length(samples)),"Factor"=markv,"Replicate"=rep("All",length(samples))
-    #skipped: check if samples,bamdir and peakdir are in the same ordera!
+if (sampleSheet == "" || is.null(sampleSheet)){
+    sampledat<-data.frame("SampleID"=samples,"Condition"=rep("All",length(samples)),"Factor"=markv,"Replicate"=rep("All",length(samples))}
+
+#skipped: check if samples,bamdir and peakdir are in the same ordera!
     
-    sampledat$bamReads<-bamdir[match(samples,sub(".filtered.bam","",basename(bamdir)))]
-    sampledat$Peaks<-peakdir[match(samples,gsub(".filtered.+","",basename(peakdir)))]
+sampledat$bamReads<-bamdir[match(samples,sub(".filtered.bam","",basename(bamdir)))]
+sampledat$Peaks<-peakdir[match(samples,gsub(".filtered.+","",basename(peakdir)))]
 
 ##annotation -> check for supported genome versions
 supported_annotations<-c("hg19"="GRCh38","hg18"="GRCh37","mm10"="GRCm38","mm9"="GRCm37","ce6"="ce6","dm3"="dm3")
