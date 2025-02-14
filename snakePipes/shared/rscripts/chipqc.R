b604662de3a106876aa77007e9fc76a66aa58993
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2025-02-14 14:19:31 +0100

    parentheses

--- snakePipes/shared/rscripts/chipqc.R
+++ snakePipes/shared/rscripts/chipqc.R
@@ -28,7 +28,7 @@ markv<-names(b)
 #if sample sheet is a file path: get condition and replicate information
 #the check that the sample sheet file exists is taken care of by the python wrapper
 if (sampleSheet == "" || is.null(sampleSheet)){
-    sampledat<-data.frame("SampleID"=samples,"Condition"=rep("All",length(samples)),"Factor"=markv,"Replicate"=rep("All",length(samples))}
+    sampledat<-data.frame("SampleID"=samples,"Condition"=rep("All",length(samples)),"Factor"=markv,"Replicate"=rep("All",length(samples)))}
 
 #skipped: check if samples,bamdir and peakdir are in the same ordera!
     
