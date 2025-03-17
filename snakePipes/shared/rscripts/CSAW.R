3ce787ca4fee2b01593dda6c56546dda80697b9c
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2025-03-17 14:15:58 +0100

    handle empty peak files csaw

--- snakePipes/shared/rscripts/CSAW.R
+++ snakePipes/shared/rscripts/CSAW.R
@@ -118,9 +118,11 @@ if (! external_bed) {
         })
     } else {
         allpeaks = lapply(snakemake@input[['peaks']], function(x) {
-            bed = read.delim(paste0("../", x), header=FALSE)
+            peakfile<-paste0("../", x)
+            if(file.exists(peakfile) & file.info(peakfile)$size > 0){
+                bed = read.delim(peakfile, header=FALSE)}else{message(paste0("Skipping peakfile ",peakfile))
             bed.gr = GRanges(seqnames = bed$V1, ranges = IRanges(start = bed$V2, end = bed$V3), name = bed$V4)
-            return(bed.gr)
+            return(bed.gr)}
         })
     }
     # merge
