74512a3390fea97a572ec6caa4dfbbc328d008f4
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2025-12-01 10:57:57 +0100

    pytest

--- tests/test_jobcounts.py
+++ tests/test_jobcounts.py
@@ -567,7 +567,7 @@ class TestDNAmapping():
         print(' '.join([str(i) for i in ci]))
         _p = sp.run(ci, capture_output=True, text=True)
         assert _p.returncode == 0
-        assert parseSpOut(_p) == 208
+        assert parseSpOut(_p) == 180
 
 class TestChIPseq:
     def test_default(self, ifs):
