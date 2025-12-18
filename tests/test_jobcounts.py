ee26fef5fc3b60d0de648cf875b3752f461b666b
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2025-12-18 11:21:34 +0100

    pytest

--- tests/test_jobcounts.py
+++ tests/test_jobcounts.py
@@ -567,7 +567,7 @@ class TestDNAmapping():
         print(' '.join([str(i) for i in ci]))
         _p = sp.run(ci, capture_output=True, text=True)
         assert _p.returncode == 0
-        assert parseSpOut(_p) == 180
+        assert parseSpOut(_p) == 184
     def test_spikein(self, ifs):
         ci = [
             "DNAmapping",
