b16db07a644eeeafb82509d8564cfbfdcfe10d29
katarzyna.otylia.sikora@gmail.com <sikora@maximus.ie-freiburg.mpg.de>
2026-04-20 09:50:56 +0200

    pytest

--- tests/test_jobcounts.py
+++ tests/test_jobcounts.py
@@ -1010,7 +1010,7 @@ class TestChIPseq:
         print(' '.join([str(i) for i in ci]))
         _p = sp.run(ci, capture_output=True, text=True)
         assert _p.returncode == 0
-        assert parseSpOut(_p) == 34
+        assert parseSpOut(_p) == 69
     def test_multicomp(self, ifs):
         ci = [
             "ChIPseq",
